890352d9c8716aa12a8cce697441f6eb7c81b890
Tim Pietrusky <timpietrusky@gmail.com>
2024-09-15 01:47:04 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Chat() {
-  return <main>Hello, world!</main>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <h1 className="text-white text-6xl">vj0</h1>
+    </div>
+  );
 }
