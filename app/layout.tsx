2b8cb35c800de321c3a6d10fcc0bb57cc83af179
Tim Pietrusky <timpietrusky@gmail.com>
2024-09-16 01:37:46 +0200

    fix: added metatags to correct position

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,25 @@ import HelpPanel from "../components/help-panel";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://vj0.live";
+
 export const metadata: Metadata = {
   title: "vj0",
   description: "generate live visual experiences with simple prompts",
+  openGraph: {
+    title: "vj0",
+    description: "generate live visual experiences with simple prompts",
+    url: SITE_URL,
+    type: "website",
+    images: [
+      {
+        url: `${SITE_URL}/preview2.png`,
+        width: 1024,
+        height: 1024,
+        alt: "A screenshot of vj0 with a generated waveform in the center and the nav in the bottom",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
